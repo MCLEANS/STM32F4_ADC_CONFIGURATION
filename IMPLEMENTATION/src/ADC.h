cbdb8db64e5d8a213c66c5a26e25973029e00572
MCLEANS <jackagwaya@gmail.com>
2020-04-09 21:04:46 +0300

    enum to declare the sampling rate

--- IMPLEMENTATION/src/ADC.h
+++ IMPLEMENTATION/src/ADC.h
@@ -3,12 +3,20 @@
  *
  *  Created on: Apr 9, 2020
  *      Author: MCLEANS
+ *
+ *      This Library expects the user to implement their own interrupt service routine
  */
 
 #ifndef ADC_H_
 #define ADC_H_
 
 namespace custom_libraries {
+
+enum sampling_rate{
+	LOW,
+	MEDIUM,
+	HIGH
+};
 
 class ADC {
 public:

384a85da215f7b035d5227780cf7a338ca7e7d46
MCLEANS <jackagwaya@gmail.com>
2020-04-09 20:51:17 +0300

    ADC configuration source file

---
+++ IMPLEMENTATION/src/ADC.cpp
@@ -0,0 +1,21 @@
+/*
+ * ADC.cpp
+ *
+ *  Created on: Apr 9, 2020
+ *      Author: MCLEANS
+ */
+
+#include "ADC.h"
+
+namespace custom_libraries {
+
+ADC::ADC() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ADC::~ADC() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace custom_libraries */

b1b159f69c109f76b4510e3e29f9cf2fea944102
MCLEANS <jackagwaya@gmail.com>
2020-04-09 20:50:37 +0300

    ADC configuration header file

---
+++ IMPLEMENTATION/src/ADC.h
@@ -0,0 +1,21 @@
+/*
+ * ADC.h
+ *
+ *  Created on: Apr 9, 2020
+ *      Author: MCLEANS
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+namespace custom_libraries {
+
+class ADC {
+public:
+	ADC();
+	~ADC();
+};
+
+} /* namespace custom_libraries */
+
+#endif /* ADC_H_ */
